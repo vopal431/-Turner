5707e1329d4973fa28ac6c36417e75326a76763c
Sebastian Müller <sebastian@web3.foundation>
2023-04-04 09:04:14 +0200

    More testing

--- src/pages/verify.js
+++ src/pages/verify.js
@@ -1,48 +1,56 @@
 import React from "react";
 import Layout from '@theme/Layout';
-// import "./styles.css";
 import Synaps from "@synaps-io/react-verify";
+import BrowserOnly from '@docusaurus/BrowserOnly';
+// import "./styles.css";
 
-export default function App() {
-  const query = new URLSearchParams(window.location.pathname);
-  const session = query.get('s');
-  const type = query.get('t');
-  console.log(session);
-  console.log(type);
-
-  const Synaps = new SynapsClient(session, type);
-  Synaps.init({
-      type: "embed",
-  })
-
+export default function verify() {
   return (
-    <Layout title="Hello" description="Hello React Page">
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>
-          Edit <code>pages/helloReact.js</code> and save to reload.
-        </p>
-      </div>
-      <div className="App">
-      <Synaps
-        sessionId={ session }
-        service={ type }
-        lang={"en"}
-        onReady={() => console.log("component ready")}
-        onFinish={() => console.log("user finish process")}
-        color={{
-          primary: "212b39",
-          secondary: "ffffff"
-        }}
-        withFinishButton={true}
-      />
-    </div>
-    </Layout>
+    <BrowserOnly>
+      {() => {
+        const query = new URLSearchParams(window.location.pathname);
+        const session = query.get('s');
+        const type = query.get('t');
+        console.log(session);
+        console.log(type);
+      
+        const Synaps = new SynapsClient(session, type);
+        Synaps.init({
+            type: "embed",
+        });
+        
+        <Layout title="Hello" description="Hello React Page">
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '50vh',
+              fontSize: '20px',
+            }}>
+            <p>
+              Edit <code>pages/helloReact.js</code> and save to reload.
+            </p>
+          </div>
+          <div className="App">
+            <Synaps
+              sessionId={ session }
+              service={ type }
+              lang={"en"}
+              onReady={() => console.log("component ready")}
+              onFinish={() => console.log("user finish process")}
+              color={{
+                primary: "212b39",
+                secondary: "ffffff"
+              }}
+              withFinishButton={true}
+            />
+          </div>
+        </Layout>
+
+      }}
+    </BrowserOnly>
+
+    
   );
 }
